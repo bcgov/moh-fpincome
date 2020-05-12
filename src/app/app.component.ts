610e1ce5c1378464ec1a821d21b0d3e3a00011ae
kristin reed <kristin.reed@maximusbc.ca>
2020-05-12 16:55:22 -0400

    Setup to run unit tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'moh-fpincome';
+  title = 'Fair PharmaCare Income Review';
 }
--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '5b2254b';
-export const buildTime = '5/11/2020 at 12:42:08 PM';
+export const gitCommit = 'c54bda1';
+export const buildTime = '5/11/2020 at 1:21:23 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.1.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 5b2254b - 5/11/2020 at 12:42:08 PM (v0.1.0) \nhttps://github.com/bcgov/moh-bcp/commits/5b2254b';
+export const message = 'Canonical Version: c54bda1 - 5/11/2020 at 1:21:23 PM (v0.1.0) \nhttps://github.com/bcgov/moh-bcp/commits/c54bda1';
 /** If true, other values should be present. */
 export const success = true;

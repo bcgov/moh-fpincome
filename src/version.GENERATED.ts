6df563d8f65f9d420ffed64233feebe68a6b5656
kristin reed <kristin.reed@maximusbc.ca>
2021-01-06 13:45:13 -0500

    chore(dependencies): update node version and dependencies

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,11 +1,11 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'b23728f';
-export const buildTime = '1/5/2021 at 5:42:11 PM';
+export const gitCommit = '57d2159';
+export const buildTime = '1/6/2021 at 10:30:03 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.0.2';
 /** Human readable message  */
 export const message =
-  'Canonical Version: b23728f - 1/5/2021 at 5:42:11 PM (v2.0.2)' +
-  '\nhttps://github.com/bcgov/moh-fpincome/commits/b23728f';
+  'Canonical Version: 57d2159 - 1/6/2021 at 10:30:03 AM (v2.0.2)' +
+  '\nhttps://github.com/bcgov/moh-fpincome/commits/57d2159';
 /** If true, other values should be present. */
 export const success = true;

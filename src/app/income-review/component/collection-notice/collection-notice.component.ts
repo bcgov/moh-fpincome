4c733bfed84977feafdc82e48095f42d9496cb2f
kristin reed <kristin.reed@maximusbc.ca>
2021-01-08 13:12:12 -0500

    fix(modal): partial fix for keeping tabbing inside dialog

--- src/app/income-review/component/collection-notice/collection-notice.component.ts
+++ src/app/income-review/component/collection-notice/collection-notice.component.ts
@@ -1,24 +1,34 @@
-import { Component, ViewChild, Input } from '@angular/core';
-import { Base } from 'moh-common-lib';
+import {
+  Component,
+  ViewChild,
+  Input,
+  ElementRef,
+  HostListener,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap';
 
 @Component({
   selector: 'fpir-collection-notice',
   templateUrl: './collection-notice.component.html',
 })
-export class CollectionNoticeComponent extends Base {
+export class CollectionNoticeComponent {
   @Input() isDisabled: boolean = false;
   @Input() buttonLabel: string = 'Continue';
 
-  @ViewChild('collectionNoticeModal', { static: true })
+  // Read after the ngAfterViewInit
+  @ViewChild('collectionNoticeModal', { static: false })
   public collectionNoticeModal: ModalDirective;
 
-  constructor() {
-    super();
-  }
+  constructor() {}
 
   public openModal(): void {
     this.collectionNoticeModal.config.keyboard = false;
+    this.collectionNoticeModal.config.focus = true;
+    this.collectionNoticeModal.config.ignoreBackdropClick = true;
+    this.collectionNoticeModal.config.backdrop = 'static';
+
     this.collectionNoticeModal.show();
   }
 
